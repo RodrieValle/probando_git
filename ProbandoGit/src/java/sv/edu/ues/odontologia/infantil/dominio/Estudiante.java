5f1fa2942465d199d44bce9efb701c5d5840f94b
Rodrigo <Rodrigo@DESKTOP-SJF5S4L>
2016-07-27 22:37:02 -0600

    Primer commit a github

---
+++ ProbandoGit/src/java/sv/edu/ues/odontologia/infantil/dominio/Estudiante.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sv.edu.ues.odontologia.infantil.dominio;
+
+/**
+ *
+ * @author Rodrigo
+ */
+public class Estudiante {
+    
+    private Integer idEstudiante;
+    private String nombre;
+
+    public Estudiante() {
+    }
+
+    public Integer getIdEstudiante() {
+        return idEstudiante;
+    }
+
+    public void setIdEstudiante(Integer idEstudiante) {
+        this.idEstudiante = idEstudiante;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+    
+}
